9eaea83fdbd36bcba0c7cbdfb3997eb9cec8e6c5
berotti3 <bero.sa.91@gmail.com>
2020-04-07 01:51:17 +0900

    fix gcj2020-c1

--- gcj2020/C1.py
+++ gcj2020/C1.py
@@ -4,13 +4,16 @@ ressort = []
 #c=0,j=1
 def comp_list(resultlist, pos, ctime, jtime):
     if pos == N:return resultlist
-
-    ressort.append(SElist[pos][2])
+    
     
     if ctime <= SElist[pos][0]:
-        resultlist = comp_list(resultlist+"C",  pos+1, SElist[pos][1], jtime)
+        resultlist.append(SElist[pos])
+        resultlist[-1].append(0)
+        resultlist = comp_list(resultlist,  pos+1, SElist[pos][1], jtime)
     elif jtime <= SElist[pos][0]:
-        resultlist = comp_list(resultlist+"J",  pos+1, ctime, SElist[pos][1])
+        resultlist.append(SElist[pos])
+        resultlist[-1].append(1)
+        resultlist = comp_list(resultlist,  pos+1, ctime, SElist[pos][1])
 
     return resultlist
 
@@ -29,31 +32,26 @@ for num in range(T):
         a.append(i)
         SElist.append(a)
 
-    #print(SElist)
     SElist.sort(key = lambda x: x[0])
 
-    #print(SElist)
 
     #Calc
     ressort = []
-    resultlist1 = comp_list("" , 0, 0, 0)
+    resultlist1 = comp_list([] , 0, 0, 0)
 
-    #print(resultlist1)
-    #print("len= " + str(len(resultlist1)))
     #output
     result = ""
 
     if len(resultlist1) != N:
         result = "IMPOSSIBLE"
     else:
-        resultlist2 = []
-        for i in range(N):
-            resultlist2.append([resultlist1[i],ressort[i]])
-
-            resultlist2.sort(key = lambda x: x[1])
+        resultlist1.sort(key = lambda x: x[2])
 
         for i in range(N):
-            result += resultlist2[i][0]
+            if resultlist1[i][3] == 0:
+                result += "C"
+            else:
+                result += "J"
 
 
 
