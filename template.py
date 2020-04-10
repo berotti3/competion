3ab7a51c2fbfacaa71bf967277d6729c29d136f2
berotti3 <bero.sa.91@gmail.com>
2020-04-11 02:28:15 +0900

    add template

---
+++ template.py
@@ -0,0 +1,14 @@
+import math
+import sys
+def getN():
+    return int(input())
+
+def getIntList():
+    return list(map(int, input().split()))
+
+def main():
+    
+    return
+
+if __name__ == "__main__":
+    main()
